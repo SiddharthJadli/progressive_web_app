9152ca181db298d64706df163bd7dd6b148741ab
Jade Loh <yloh0013@student.monash.edu>
2023-09-21 23:54:01 +1000

    files, add/list

--- assignment-3/src/app/services/database.service.ts
+++ assignment-3/src/app/services/database.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http"
+const backendBaseUrl = 'http://localhost:8080';
 
 const httpOptions = {
     headers: new HttpHeaders(
@@ -13,11 +14,11 @@ export class DatabaseService {
     result : any;
 
     getCategory() {
-        return this.http.get('/add-category')
+        return this.http.get(`${backendBaseUrl}/list-category`)
     }
 
     addCategory(aCategory : any) {
-        return this.http.post("/add-category", aCategory, httpOptions)
+        return this.http.post(`${backendBaseUrl}/add-category`, aCategory, httpOptions)
     }
 
     deleteCategory(catId : string) {
